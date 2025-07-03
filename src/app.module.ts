1fdc512970cd4dff3938ffe34aa9758d9faffd1f
Akamnex666 <eliisaruiz775@gmail.com>
2025-07-03 05:58:31 -0500

    iu

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { AuthModule } from './auth/auth.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
+        // synchronize: true,
       }),
       inject: [ConfigService],
     }),
